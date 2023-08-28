2b0dc8641276b60574e9e8d244323e179d986604
Djothi <d.carpentier@criteo.com>
2023-08-28 16:47:31 +0200

    feat: add VLAN model

--- netbox_cmdb/netbox_cmdb/models/__init__.py
+++ netbox_cmdb/netbox_cmdb/models/__init__.py
@@ -3,4 +3,5 @@ from netbox_cmdb.models.bgp_community_list import *
 from netbox_cmdb.models.circuit import *
 from netbox_cmdb.models.prefix_list import *
 from netbox_cmdb.models.route_policy import *
+from netbox_cmdb.models.vlan import *
 from netbox_cmdb.models.vrf import *
