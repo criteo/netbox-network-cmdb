80f8140301c89b537c1e4f7a6e1c8457edd5c714
Djothi <d.carpentier@criteo.com>
2023-09-04 17:05:50 +0200

    feat: add LogicalInterface model

--- netbox_cmdb/netbox_cmdb/models/interface.py
+++ netbox_cmdb/netbox_cmdb/models/interface.py
@@ -8,6 +8,17 @@ FEC_CHOICES = [
     ("fc", "FireCode"),
 ]
 
+LOGICAL_INTERFACE_TYPE_CHOICES = [
+    ("l1", "L1"),
+    ("l2", "L2"),
+    ("l3", "L3"),
+]
+LOGICAL_INTERFACE_MODE_CHOICES = [
+    (None, "None"),
+    ("access", "Access"),
+    ("tagged", "Tagged"),
+]
+
 
 class DeviceInterface(ChangeLoggedModel):
     """A device interface configuration."""
@@ -40,3 +51,73 @@ class DeviceInterface(ChangeLoggedModel):
 
     class Meta:
         unique_together = ("device", "name")
+
+
+class LogicalInterface(ChangeLoggedModel):
+    """A logical interface configuration."""
+
+    index = models.PositiveSmallIntegerField()
+    parent_interface = models.ForeignKey(
+        to="DeviceInterface",
+        related_name="%(class)s",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    mtu = models.PositiveIntegerField(blank=True, null=True)
+    type = models.CharField(
+        choices=LOGICAL_INTERFACE_TYPE_CHOICES,
+        max_length=2,
+        default=None,
+    )
+    vrf = models.ForeignKey(
+        to="VRF", related_name="%(class)s_vrf", on_delete=models.CASCADE, blank=True, null=True
+    )
+    ipv4_address = models.ForeignKey(
+        to="ipam.IPAddress",
+        related_name="%(class)s_ipv4_address",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    ipv6_address = models.ForeignKey(
+        to="ipam.IPAddress",
+        related_name="%(class)s_ipv6_address",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    mode = models.CharField(
+        choices=LOGICAL_INTERFACE_MODE_CHOICES,
+        blank=True,
+        null=True,
+        default=None,
+        max_length=20,
+        help_text="Interface mode (802.1Q)",
+    )
+    untagged_vlan = models.ForeignKey(
+        to="VLAN",
+        related_name="%(class)s_untagged_vlan",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        default=None,
+    )
+    tagged_vlans = models.ManyToManyField(
+        to="VLAN", related_name="%(class)s_tagged_vlans", blank=True, default=None
+    )
+    native_vlan = models.ForeignKey(
+        to="VLAN",
+        related_name="%(class)s_native_vlan",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        default=None,
+    )
+    description = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.parent_interface.name}--{self.index}"
+
+    class Meta:
+        unique_together = ("index", "parent_interface")
