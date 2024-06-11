83294dc40304dc10fe6933e446a5ae0e095987d5
Cedric Paillet <c.paillet@criteo.com>
2024-06-11 15:22:14 +0200

    fix(snmp): enhance API output for detailed device info
    Enhanced the /api/plugins/cmdb/snmp/ endpoint to include detailed
    information for devices and communities, not just the primary key (pk).

--- netbox_cmdb/netbox_cmdb/api/snmp/serializers.py
+++ netbox_cmdb/netbox_cmdb/api/snmp/serializers.py
@@ -3,6 +3,7 @@
 from rest_framework.serializers import ModelSerializer
 
 from netbox_cmdb.models.snmp import SNMP, SNMPCommunity
+from netbox_cmdb.api.common_serializers import CommonDeviceSerializer
 
 
 class SNMPCommunitySerializer(ModelSerializer):
@@ -12,7 +13,26 @@ class SNMPCommunitySerializer(ModelSerializer):
         fields = "__all__"
 
 
+class SNMPCommunityReadSerializer(ModelSerializer):
+
+    class Meta:
+        model = SNMPCommunity
+        fields = ["community", "type"]
+
+
+class SNMPReadSerializer(ModelSerializer):
+
+    device = CommonDeviceSerializer()
+    community_list = SNMPCommunityReadSerializer(many=True)
+
+    class Meta:
+        model = SNMP
+        fields = "__all__"
+
+
 class SNMPSerializer(ModelSerializer):
+
+    device = CommonDeviceSerializer()
 
     class Meta:
         model = SNMP
