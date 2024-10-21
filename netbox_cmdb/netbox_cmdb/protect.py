8ea40dc2f9cce77c78df87230ddb31243eb1cb2f
k.petremann <k.petremann@criteo.com>
2024-10-21 14:58:15 +0200

    docs(protect): docstring for device/ipam protection

--- netbox_cmdb/netbox_cmdb/protect.py
+++ netbox_cmdb/netbox_cmdb/protect.py
@@ -3,6 +3,11 @@ MODELS_LINKED_TO_IP_ADDRESS = {}
 
 
 def from_device_name_change(*fields):
+    """Protects from Device name changes in NetBox DCIM.
+
+    This is useful only to prevent Device name changes when CMDB is linked to it.
+    """
+
     def decorator(cls):
         if cls not in MODELS_LINKED_TO_DEVICE:
             MODELS_LINKED_TO_DEVICE[cls] = set()
@@ -19,6 +24,11 @@ def from_device_name_change(*fields):
 
 
 def from_ip_address_change(*fields):
+    """Protects from IP Address "address" changes in NetBox IPAM.
+
+    This is useful only to prevent IP Address "address" changes when CMDB is linked to it.
+    """
+
     def decorator(cls):
         if cls not in MODELS_LINKED_TO_IP_ADDRESS:
             MODELS_LINKED_TO_IP_ADDRESS[cls] = set()
