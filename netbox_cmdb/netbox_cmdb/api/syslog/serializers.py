2e4a5b8740ca127f2a4a17be4a92ab33fa5bdfa9
Thierry Kieffer <t.kieffer@criteo.com>
2026-01-07 16:58:25 +0100

    fix(api): rework syslog

--- netbox_cmdb/netbox_cmdb/api/syslog/serializers.py
+++ netbox_cmdb/netbox_cmdb/api/syslog/serializers.py
@@ -1,5 +1,6 @@
 """Syslog serializers."""
 
+from dcim.models import Device
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
@@ -30,9 +31,10 @@ class SyslogServerReadSerializer(ModelSerializer):
 class SyslogSerializer(ModelSerializer):
     """
     Serializer used for write/create/update operations.
+    One Syslog configuration per device, with multiple Syslog servers.
     """
 
-    device = CommonDeviceSerializer(read_only=True)
+    device = CommonDeviceSerializer()
 
     # Writable list of FK (PK list)
     server_list = serializers.PrimaryKeyRelatedField(
@@ -42,6 +44,51 @@ class SyslogSerializer(ModelSerializer):
     class Meta:
         model = Syslog
         fields = "__all__"
+
+    def create(self, validated_data):
+        servers = validated_data.pop("server_list", [])
+        device_data = validated_data.pop("device")
+
+        # If Device is already an object, use it
+        if isinstance(device_data, Device):
+            device = device_data
+        # Elif it's a Dict with 'id' or 'name'
+        elif isinstance(device_data, dict):
+            if "id" in device_data:
+                device = Device.objects.get(pk=device_data["id"])
+            elif "name" in device_data:
+                device = Device.objects.get(name=device_data["name"])
+            else:
+                raise serializers.ValidationError("Device must have 'id' or 'name'.")
+        else:
+            raise serializers.ValidationError("Invalid device data")
+
+        syslog, created = Syslog.objects.get_or_create(device=device)
+        syslog.server_list.set(servers)
+        return syslog
+
+    def update(self, instance, validated_data):
+        servers = validated_data.pop("server_list", None)
+        device_data = validated_data.pop("device", None)
+
+        if device_data:
+            if isinstance(device_data, Device):
+                instance.device = device_data
+            elif isinstance(device_data, dict):
+                if "id" in device_data:
+                    instance.device = Device.objects.get(pk=device_data["id"])
+                elif "name" in device_data:
+                    instance.device = Device.objects.get(name=device_data["name"])
+                else:
+                    raise serializers.ValidationError("Device must have 'id' or 'name'.")
+            else:
+                raise serializers.ValidationError("Invalid device data")
+            instance.save()
+
+        if servers is not None:
+            instance.server_list.set(servers)
+
+        return instance
 
 
 class SyslogReadSerializer(ModelSerializer):
--- netbox_cmdb/netbox_cmdb/tests/syslog/test_syslog_serializer.py
+++ netbox_cmdb/netbox_cmdb/tests/syslog/test_syslog_serializer.py
@@ -1,71 +1,61 @@
-from netbox_cmdb.api.syslog.serializers import (
-    SyslogSerializer,
-    SyslogServerSerializer,
-)
+from netbox_cmdb.api.syslog.serializers import SyslogSerializer, SyslogServerSerializer
 from netbox_cmdb.models.syslog import Syslog, SyslogServer
 from netbox_cmdb.tests.common import BaseTestCase
 
 
 class SyslogSerializerCreate(BaseTestCase):
 
-    def test_create_syslog_server_and_assign_to_device(self):
+    def test_create_and_update_syslog_servers(self):
         """
-        Create SyslogServer with only server_address,
-        then create Syslog and assign servers to devices.
+        Test creating SyslogServers and assigning them to devices,
+        including updating server_list for existing Syslog.
         """
 
-        # Create a SyslogServer
-        server_data = {
-            "server_address": "10.10.10.1",
-        }
+        # --- Create SyslogServer 1 ---
+        server_data1 = {"server_address": "10.10.10.1"}
+        server_serializer1 = SyslogServerSerializer(data=server_data1)
+        assert server_serializer1.is_valid() is True
+        server_serializer1.save()
+        server1 = SyslogServer.objects.get(server_address="10.10.10.1")
 
-        server_serializer = SyslogServerSerializer(data=server_data)
-        assert server_serializer.is_valid() is True
-        server_serializer.save()
-
-        server_obj = SyslogServer.objects.get(server_address="10.10.10.1")
-
-        assert server_obj.server_address == "10.10.10.1"
-
-        # Create Syslog entry for a device
-        syslog_data = {
-            "device": self.device1.pk,
-            "server_list": [server_obj.pk],
-        }
-
-        syslog_serializer = SyslogSerializer(data=syslog_data)
-        assert syslog_serializer.is_valid() is True
-        syslog_serializer.save()
-
-        syslog_obj = Syslog.objects.get(device=self.device1)
-
-        assert syslog_obj.device == self.device1
-        assert syslog_obj.server_list.count() == 1
-        assert syslog_obj.server_list.first() == server_obj
-
-        # Create a second SyslogServer
-        server_data2 = {
-            "server_address": "10.10.10.2",
-        }
-
+        # --- Create SyslogServer 2 ---
+        server_data2 = {"server_address": "10.10.10.2"}
         server_serializer2 = SyslogServerSerializer(data=server_data2)
         assert server_serializer2.is_valid() is True
         server_serializer2.save()
+        server2 = SyslogServer.objects.get(server_address="10.10.10.2")
 
-        server_obj2 = SyslogServer.objects.get(server_address="10.10.10.2")
+        # --- Create Syslog for device1 using device ID ---
+        syslog_data1 = {"device": {"id": self.device1.pk}, "server_list": [server1.pk]}
+        syslog_serializer1 = SyslogSerializer(data=syslog_data1)
+        assert syslog_serializer1.is_valid() is True
+        syslog_serializer1.save()
+        syslog_obj1 = Syslog.objects.get(device=self.device1)
 
-        # Assign two servers to a second device
-        syslog_data2 = {
-            "device": self.device2.pk,
-            "server_list": [server_obj.pk, server_obj2.pk],
-        }
+        assert syslog_obj1.device == self.device1
+        assert syslog_obj1.server_list.count() == 1
+        assert syslog_obj1.server_list.first() == server1
 
+        # --- Create Syslog for device2 using device ID with 2 servers ---
+        syslog_data2 = {"device": {"id": self.device2.pk}, "server_list": [server1.pk, server2.pk]}
         syslog_serializer2 = SyslogSerializer(data=syslog_data2)
         assert syslog_serializer2.is_valid() is True
         syslog_serializer2.save()
-
         syslog_obj2 = Syslog.objects.get(device=self.device2)
 
         assert syslog_obj2.server_list.count() == 2
-        assert server_obj in syslog_obj2.server_list.all()
-        assert server_obj2 in syslog_obj2.server_list.all()
+        assert server1 in syslog_obj2.server_list.all()
+        assert server2 in syslog_obj2.server_list.all()
+
+        # --- Update existing Syslog for device2 using device name ---
+        syslog_data_update = {"device": {"name": "router-test2"}, "server_list": [server1.pk]}
+        # Get existing Syslog instance
+        syslog_obj2 = Syslog.objects.get(device=self.device2)
+        syslog_serializer_update = SyslogSerializer(instance=syslog_obj2, data=syslog_data_update)
+        assert syslog_serializer_update.is_valid() is True
+        syslog_serializer_update.save()
+        syslog_obj2.refresh_from_db()
+
+        assert syslog_obj2.device == self.device2
+        assert syslog_obj2.server_list.count() == 1
+        assert syslog_obj2.server_list.first() == server1
